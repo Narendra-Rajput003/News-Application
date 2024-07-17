f063439fba4068a66859ad2ec5d718ced1a31b69
Narendra-Rajput003 <narendrarajput05007@gmail.com>
2024-07-17 23:18:34 +0530

    "Update rate limiter max requests from 10 to 1200 per window"

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -8,7 +8,7 @@ const rateLimiter = rateLimitMiddleware({
         sendCommand: (...args) => redis.call(...args) // Use ioredis's call method
     }),
     windowMs: 60 * 1000, // 1 minute window
-    max: 10, // Maximum 10 requests per window
+    max: 1200, // Maximum 10 requests per window
     message: "Too many requests from this IP, please try again after some time.", // Custom message for rate limit exceeded
     statusCode: 429, // HTTP status code for rate limit exceeded
     keyGenerator: (req) => req.ip, // Use IP address as the key for rate limiting
